3616e7cc2433d2a4d45a8e6cebfc97566059ad5e
zt <960686470@qq.com>
2023-11-17 23:27:44 +0800

    feat: Add Course classification selection

--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseCategoryController.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseCategoryController.java
@@ -1,7 +1,11 @@
 package com.xuecheng.content.api;
 
 import com.xuecheng.content.model.dto.CourseCategoryTreeDto;
+import com.xuecheng.content.service.CourseCategoryService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,11 +21,16 @@ import java.util.List;
 
 @Slf4j
 @RestController
+@Api(tags = "课程分类")
 public class CourseCategoryController {
 
+    @Autowired
+    CourseCategoryService courseCategoryService;
+
+    @ApiOperation(value = "课程分类选择")
     @GetMapping("/course-category/tree-nodes")
     public List<CourseCategoryTreeDto> queryTreeNodes () {
-        return null;
+        return courseCategoryService.queryTreeNodes("1");
     }
 
 }
