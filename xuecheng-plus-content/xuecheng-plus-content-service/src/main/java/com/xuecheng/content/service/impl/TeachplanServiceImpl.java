6d757df28947f5364b62f69044a82f0baf358eac
zt <960686470@qq.com>
2023-11-20 23:52:44 +0800

    feat: Add Course section controller

--- xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/impl/TeachplanServiceImpl.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/impl/TeachplanServiceImpl.java
@@ -1,10 +1,16 @@
 package com.xuecheng.content.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.xuecheng.content.mapper.TeachplanMapper;
+import com.xuecheng.content.model.dto.SaveTeachplanDto;
 import com.xuecheng.content.model.dto.TeachplanDto;
+import com.xuecheng.content.model.po.Teachplan;
 import com.xuecheng.content.service.TeachplanService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -25,4 +31,40 @@ public class TeachplanServiceImpl implements TeachplanService {
     public List<TeachplanDto> findTeachplanTree(Long courseId) {
         return teachplanMapper.selectTreeNodes(courseId);
     }
+
+    @Transactional
+    @Override
+    public void saveTeachplan(SaveTeachplanDto teachplanDto) {
+        //课程计划id
+        Long id = teachplanDto.getId();
+        //修改课程计划
+        if (id !=null) {
+            Teachplan teachplan = teachplanMapper.selectById(id);
+            BeanUtils.copyProperties(teachplanDto, teachplan);
+            teachplanMapper.updateById(teachplan);
+        } else {
+            //取出同父同级别的课程计划数量
+            int count = getTeachplanCount(teachplanDto.getCourseId(), teachplanDto.getParentid());
+            Teachplan teachplanNew = new Teachplan();
+            //设置排序号
+            teachplanNew.setOrderby(count + 1);
+            BeanUtils.copyProperties(teachplanDto,teachplanNew);
+            teachplanMapper.insert(teachplanNew);
+        }
+    }
+
+    /**
+     * 获取最新的排序号
+     * @param courseId 课程id
+     * @param parentid 父课程计划id
+     * @return int 最新排序号
+     */
+    private int getTeachplanCount(Long courseId, Long parentid) {
+        LambdaQueryWrapper<Teachplan> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(Teachplan::getCourseId, courseId);
+        queryWrapper.eq(Teachplan::getParentid, parentid);
+        Integer count = teachplanMapper.selectCount(queryWrapper);
+        return count;
+    }
+
 }
