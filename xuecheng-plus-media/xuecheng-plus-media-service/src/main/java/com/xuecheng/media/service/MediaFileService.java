52090e0728dea926e01b8a4c38e7c9fcb1f123cf
zt <960686470@qq.com>
2023-11-26 22:50:13 +0800

    feat: 增加上传视频接口,支持断点续传

--- xuecheng-plus-media/xuecheng-plus-media-service/src/main/java/com/xuecheng/media/service/MediaFileService.java
+++ xuecheng-plus-media/xuecheng-plus-media-service/src/main/java/com/xuecheng/media/service/MediaFileService.java
@@ -2,42 +2,90 @@ package com.xuecheng.media.service;
 
 import com.xuecheng.base.model.PageParams;
 import com.xuecheng.base.model.PageResult;
+import com.xuecheng.base.model.RestResponse;
 import com.xuecheng.media.model.dto.QueryMediaParamsDto;
 import com.xuecheng.media.model.dto.UploadFileParamsDto;
 import com.xuecheng.media.model.dto.UploadFileResultDto;
 import com.xuecheng.media.model.po.MediaFiles;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 /**
- * @description 媒资文件管理业务类
  * @author Mr.M
- * @date 2022/9/10 8:55
  * @version 1.0
+ * @description 媒资文件管理业务类
+ * @date 2022/9/10 8:55
  */
 public interface MediaFileService {
 
- /**
-  * @description 媒资文件查询方法
-  * @param pageParams 分页参数
-  * @param queryMediaParamsDto 查询条件
-  * @return com.xuecheng.base.model.PageResult<com.xuecheng.media.model.po.MediaFiles>
-  * @author Mr.M
-  * @date 2022/9/10 8:57
- */
- public PageResult<MediaFiles> queryMediaFiels(Long companyId,PageParams pageParams, QueryMediaParamsDto queryMediaParamsDto);
+    /**
+     * @param pageParams          分页参数
+     * @param queryMediaParamsDto 查询条件
+     * @return com.xuecheng.base.model.PageResult<com.xuecheng.media.model.po.MediaFiles>
+     * @description 媒资文件查询方法
+     * @author Mr.M
+     * @date 2022/9/10 8:57
+     */
+    PageResult<MediaFiles> queryMediaFiels(Long companyId, PageParams pageParams, QueryMediaParamsDto queryMediaParamsDto);
 
 
- /**
-  * 上传文件
-  * @param companyId 机构Id
-  * @param uploadFileParamsDto 上传文件信息
-  * @param localFilePath 文件磁盘路径
-  * @return
-  */
- public UploadFileResultDto uploadFile(Long companyId, UploadFileParamsDto uploadFileParamsDto, String localFilePath);
+    /**
+     * 上传文件
+     *
+     * @param companyId           机构Id
+     * @param uploadFileParamsDto 上传文件信息
+     * @param localFilePath       文件磁盘路径
+     * @return
+     */
+    UploadFileResultDto uploadFile(Long companyId, UploadFileParamsDto uploadFileParamsDto, String localFilePath);
 
 
- public  MediaFiles addMediaFilesToDb(Long companyId, String fileMd5, UploadFileParamsDto uploadFileParamsDto, String bucket, String objectName);
+    /**
+     * @param companyId           机构Id
+     * @param fileMd5             文件Md5值
+     * @param uploadFileParamsDto
+     * @param bucket
+     * @param objectName
+     * @return
+     */
+    MediaFiles addMediaFilesToDb(Long companyId, String fileMd5, UploadFileParamsDto uploadFileParamsDto, String bucket, String objectName);
+
+    /**
+     * 检查文件是否存在
+     *
+     * @param fileMd5 文件的md5值
+     * @return
+     */
+    RestResponse<Boolean> checkFile(String fileMd5);
+
+    /**
+     * 检查分块是否存在
+     * @param fileMd5 文件的md5值
+     * @param chunkIndex 分块序号
+     * @return
+     */
+    RestResponse<Boolean> checkChunk(String fileMd5, int chunkIndex);
+
+    /**
+     * 上传文件分块
+     * @param fileMd5 文件md5
+     * @param chunk 分块序号
+     * @param localChunkFilePath 分块文件本地路径
+     * @return
+     */
+    RestResponse uploadChunk(String fileMd5, int chunk, String localChunkFilePath);
+
+
+    /**
+     * 合并分块文件
+     * @param companyId 机构id
+     * @param fileMd5 文件Md5值
+     * @param chunkTotal 文件分块总和
+     * @param uploadFileParamsDto 文件信息
+     * @return
+     */
+    RestResponse mergechunks(Long companyId,String fileMd5,int chunkTotal,UploadFileParamsDto uploadFileParamsDto);
 }
